a888d78fc24849edd22e3410fe5c62d9606851c2
Antonio Chavez <cavjzz@gmail.com>
2012-02-16 18:49:39 -0600

    Created users controller with new, create, and delete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def main
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    User.create(params[:user])
+    redirect_to "/users"
+  end
+
+  def delete
+    User.find(params[:id]).delete
+    redirect_to "/users"
+  end
+end
